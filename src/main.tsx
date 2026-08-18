090e87a5d94e227a49643811e35bcd4d83d05e77
caua474 <cauafffelipedacosta@gmail.com>
2026-08-18 12:27:27 -0300

    Remove service worker do main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { registerServiceWorker } from './utils/serviceWorker.ts';
+
 
 // Register Service Worker for offline reading
-registerServiceWorker();
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
