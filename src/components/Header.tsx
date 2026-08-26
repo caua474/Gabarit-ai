6a34078461a1ce8dc8f23e9f76c208c96c2c56a3
caua474 <cauafffelipedacosta@gmail.com>
2026-08-26 11:54:10 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
+
+export const Header: React.FC = () => {
+  return (
+    <header style={{ padding: '20px', textAlign: 'center', background: '#111', color: '#fff' }}>
+      <h1>Gabarita-AI</h1>
+    </header>
+  );
+};
+
+export default Header;
 
