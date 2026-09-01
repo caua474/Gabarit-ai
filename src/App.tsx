805af3dda82439b31b123030455bbe6a22a0a39d
caua474 <cauafffelipedacosta@gmail.com>
2026-09-01 18:39:56 -0300

    Remove unused import of PerfilXP component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import PerfilXP from './components/PerfilXP';
+import React, { useState } from 'react';
 import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { OfflineStatusBanner } from './components/OfflineStatusBanner';
