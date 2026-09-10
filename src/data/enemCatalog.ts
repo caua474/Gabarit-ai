05166ed27cbf4e118e87d2cd984a52ce7dc42d99
caua474 <cauafffelipedacosta@gmail.com>
2026-09-10 17:41:01 -0300

    Add SubjectArea interface and enemCatalog data

--- src/data/enemCatalog.ts
+++ src/data/enemCatalog.ts
@@ -1,1 +1,31 @@
+export interface SubjectArea {
+  id: string;
+  title: string;
+  topics: string[];
+}
+
+export const enemCatalog: SubjectArea[] = [
+  {
+    id: "linguagens",
+    title: "Linguagens e Códigos",
+    topics: ["Interpretação de Texto", "Funções da Linguagem", "Literatura", "Gramática"]
+  },
+  {
+    id: "humanas",
+    title: "Ciências Humanas",
+    topics: ["História do Brasil", "Geografia Geral", "Filosofia", "Sociologia"]
+  },
+  {
+    id: "natureza",
+    title: "Ciências da Natureza",
+    topics: ["Física - Mecânica", "Química Geral", "Biologia - Ecologia"]
+  },
+  {
+    id: "matematica",
+    title: "Matemática",
+    topics: ["Geometria", "Funções", "Estatística", "Porcentagem"]
+  }
+];
+
+export default enemCatalog;
 
